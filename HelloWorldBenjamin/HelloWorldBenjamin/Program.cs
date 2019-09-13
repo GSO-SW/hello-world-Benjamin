860dc7efd41384e67098e4be285032aa778a0239
Benjamin Osmani <benjamino@schule.koeln>
2019-09-13 12:30:19 +0200

    Hello World wird auf auf der Konsole ausgeführt

--- HelloWorldBenjamin/HelloWorldBenjamin/Program.cs
+++ HelloWorldBenjamin/HelloWorldBenjamin/Program.cs
@@ -10,6 +10,8 @@ namespace HelloWorldBenjamin
     {
         static void Main(string[] args)
         {
+            Console.Write("hELlO wOrLd");
+            Console.ReadLine();
         }
     }
 }
